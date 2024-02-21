faf11fb2082e2fa17abd8845fa7edc0b8234479e
PabloJL <jup_jim_7@hotmail.com>
2024-02-20 19:15:22 -0600

    Auth working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,17 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const adapter = PrismaAdapter(prisma);
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+export const authOptions = {
+  adapter: adapter,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);

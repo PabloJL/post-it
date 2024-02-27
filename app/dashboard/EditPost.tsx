6c5fd965025ecbec5c10dbcce28163640c3e848c
PabloJL <jup_jim_7@hotmail.com>
2024-02-26 18:35:58 -0600

    Edit post ui added

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -17,7 +17,7 @@ type EditProps = {
 function EditPost({ image, name, title, comments, id }: EditProps) {
   return (
     <div className="  bg-white my-8 p-8 rounded-lg">
-      <div>
+      <div className=" flex items-center gap-2">
         <Image
           width={32}
           height={32}
@@ -27,6 +27,15 @@ function EditPost({ image, name, title, comments, id }: EditProps) {
           priority
         />
         <h3 className=" font-bold text-gray-700">{name}</h3>
+      </div>
+      <div className=" my-8">
+        <p className=" break-all">{title}</p>
+      </div>
+      <div className=" flex items-center gap-4">
+        <p className=" text-sm font-bold text-gray-700">
+          {comments?.length} Comments
+        </p>
+        <button className=" text-sm font-bold text-red-500">Delete</button>
       </div>
     </div>
   );

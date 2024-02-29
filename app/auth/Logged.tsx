63e5a62b6a65a9779c14e0863a1039e90181663c
PabloJL <jup_jim_7@hotmail.com>
2024-02-29 16:42:15 -0600

    Added border to profile menu

--- app/auth/Logged.tsx
+++ app/auth/Logged.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Image, from "next/image";
+import Image from "next/image";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
@@ -7,7 +7,6 @@ import { useState } from "react";
 type User = {
   image: string;
 };
-
 
 function Logged({ image }: User) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,26 +18,35 @@ function Logged({ image }: User) {
   return (
     <li className=" flex gap-8 items-center">
       <div className=" relative">
-      <div onClick={toggleMenu}>
-        <Image
-          width={50}
-          height={50}
-          src={image}
-          alt="profile picture"
-          className="rounded-full"
-          priority
-        />
-      </div>
-      {isMenuOpen && (
-        <div className="menu absolute top-full right-2 mt-1 w-40 bg-white  p-8 rounded-lg shadow-sm text-end ">
-          {/* Menu content goes here */}
-          <ul>
-            <li className=" font-normal"><Link href={"/dashboard"} onClick={toggleMenu}>Profile</Link></li>
-            <li className=" cursor-pointer text-red-600 mt-2" onClick={()=>signOut()}>Sign out</li>
-          </ul>
+        <div onClick={toggleMenu}>
+          <Image
+            width={50}
+            height={50}
+            src={image}
+            alt="profile picture"
+            className="rounded-full"
+            priority
+          />
         </div>
-      )}
-    </div>
+        {isMenuOpen && (
+          <div className="menu absolute top-full right-2 mt-1 w-40 bg-white  p-8 rounded-lg shadow-sm text-end ">
+            {/* Menu content goes here */}
+            <ul>
+              <li className=" font-normal border-b border-gray-700 pb-1">
+                <Link href={"/dashboard"} onClick={toggleMenu}>
+                  Profile
+                </Link>
+              </li>
+              <li
+                className=" cursor-pointer text-red-600 mt-2"
+                onClick={() => signOut()}
+              >
+                Sign out
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </li>
   );
 }
